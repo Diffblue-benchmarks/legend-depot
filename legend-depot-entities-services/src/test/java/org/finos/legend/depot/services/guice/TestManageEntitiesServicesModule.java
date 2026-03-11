b24f8976b559e90b59e921474174c7a06755dc60
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 14:39:19 +0000

    test(legend-depot-entities-services): add generated tests for org.finos.legend.depot.services.guice.ManageEntitiesServicesModule.configure()V

---
+++ legend-depot-entities-services/src/test/java/org/finos/legend/depot/services/guice/TestManageEntitiesServicesModule.java
@@ -0,0 +1,56 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.services.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.finos.legend.depot.services.api.entities.EntitiesService;
+import org.finos.legend.depot.services.api.entities.EntityClassifierService;
+import org.finos.legend.depot.services.api.entities.ManageEntitiesService;
+import org.finos.legend.depot.services.api.projects.ProjectsService;
+import org.finos.legend.depot.store.api.entities.Entities;
+import org.finos.legend.depot.store.api.entities.UpdateEntities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class TestManageEntitiesServicesModule
+{
+    @Test
+    public void canInitialiseManageEntitiesServicesModule()
+    {
+        UpdateEntities mockUpdateEntities = mock(UpdateEntities.class);
+        Injector injector = Guice.createInjector(
+                new AbstractModule()
+                {
+                    @Override
+                    protected void configure()
+                    {
+                        bind(UpdateEntities.class).toInstance(mockUpdateEntities);
+                        bind(Entities.class).toInstance(mockUpdateEntities);
+                        bind(ProjectsService.class).toInstance(mock(ProjectsService.class));
+                    }
+                },
+                new ManageEntitiesServicesModule()
+        );
+
+        Assertions.assertNotNull(injector.getInstance(ManageEntitiesService.class));
+        Assertions.assertNotNull(injector.getInstance(EntitiesService.class));
+        Assertions.assertNotNull(injector.getInstance(EntityClassifierService.class));
+    }
+}
