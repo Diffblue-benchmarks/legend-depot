60f3722c433266642394fbca4b0ac951721ed950
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 13:20:45 +0000

    test(legend-depot-model): add generated tests for org.finos.legend.depot.domain.CoordinateData
    - <init>
    - <init>
    - getGroupId
    - getArtifactId
    - setArtifactId
    - equals
    - hashCode

---
+++ legend-depot-model/src/test/java/org/finos/legend/depot/domain/CoordinateDataTest.java
@@ -0,0 +1,76 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CoordinateDataTest
+{
+    private static class TestCoordinateData extends CoordinateData
+    {
+        public TestCoordinateData()
+        {
+            super();
+        }
+
+        public TestCoordinateData(String groupId, String artifactId)
+        {
+            super(groupId, artifactId);
+        }
+    }
+
+    @Test
+    public void testDefaultConstructor()
+    {
+        TestCoordinateData data = new TestCoordinateData();
+        Assertions.assertNull(data.getGroupId());
+        Assertions.assertNull(data.getArtifactId());
+    }
+
+    @Test
+    public void testParameterizedConstructor()
+    {
+        TestCoordinateData data = new TestCoordinateData("org.finos", "legend-depot");
+        Assertions.assertEquals("org.finos", data.getGroupId());
+        Assertions.assertEquals("legend-depot", data.getArtifactId());
+    }
+
+    @Test
+    public void testSetArtifactId()
+    {
+        TestCoordinateData data = new TestCoordinateData("org.finos", "legend-depot");
+        data.setArtifactId("new-artifact");
+        Assertions.assertEquals("new-artifact", data.getArtifactId());
+    }
+
+    @Test
+    public void testEqualsAndHashCode()
+    {
+        TestCoordinateData data1 = new TestCoordinateData("org.finos", "legend-depot");
+        TestCoordinateData data2 = new TestCoordinateData("org.finos", "legend-depot");
+        Assertions.assertEquals(data1, data2);
+        Assertions.assertEquals(data1.hashCode(), data2.hashCode());
+    }
+
+    @Test
+    public void testNotEquals()
+    {
+        TestCoordinateData data1 = new TestCoordinateData("org.finos", "legend-depot");
+        TestCoordinateData data2 = new TestCoordinateData("org.finos", "other-artifact");
+        Assertions.assertNotEquals(data1, data2);
+    }
+}
