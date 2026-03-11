d8005109defc52b79c300c7981a96a2fcb076e79
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 15:05:12 +0000

    test(legend-depot-artifacts-services): add generated tests for org.finos.legend.depot.services.guice.ArtifactsServicesModule.configureHandlers()V

--- legend-depot-artifacts-services/src/test/java/org/finos/legend/depot/services/guice/TestArtifactsServicesModule.java
+++ legend-depot-artifacts-services/src/test/java/org/finos/legend/depot/services/guice/TestArtifactsServicesModule.java
@@ -15,14 +15,32 @@
 
 package org.finos.legend.depot.services.guice;
 
+import com.google.inject.spi.Element;
+import com.google.inject.spi.Elements;
+import com.google.inject.spi.LinkedKeyBinding;
+import com.google.inject.spi.PrivateElements;
 import org.finos.legend.depot.core.services.api.metrics.PrometheusMetricsHandler;
 import org.finos.legend.depot.domain.artifacts.repository.ArtifactType;
 import org.finos.legend.depot.services.api.artifacts.configuration.ArtifactsRetentionPolicyConfiguration;
 import org.finos.legend.depot.services.api.artifacts.handlers.ProjectArtifactHandlerFactory;
 import org.finos.legend.depot.services.api.artifacts.handlers.entties.EntitiesArtifactsHandler;
+import org.finos.legend.depot.services.api.artifacts.handlers.entties.EntityArtifactsProvider;
+import org.finos.legend.depot.services.api.artifacts.handlers.entties.VersionedEntitiesArtifactsHandler;
+import org.finos.legend.depot.services.api.artifacts.handlers.entties.VersionedEntityArtifactsProvider;
 import org.finos.legend.depot.services.api.artifacts.handlers.generations.FileGenerationsArtifactsHandler;
+import org.finos.legend.depot.services.api.artifacts.handlers.generations.FileGenerationsArtifactsProvider;
+import org.finos.legend.depot.services.artifacts.handlers.entities.EntitiesHandlerImpl;
+import org.finos.legend.depot.services.artifacts.handlers.entities.EntityProvider;
+import org.finos.legend.depot.services.artifacts.handlers.entities.VersionedEntitiesHandlerImpl;
+import org.finos.legend.depot.services.artifacts.handlers.entities.VersionedEntityProvider;
+import org.finos.legend.depot.services.artifacts.handlers.generations.FileGenerationHandlerImpl;
+import org.finos.legend.depot.services.artifacts.handlers.generations.FileGenerationsProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -78,5 +96,30 @@ public class TestArtifactsServicesModule
         ArtifactsRetentionPolicyConfiguration config = new ArtifactsRetentionPolicyConfiguration(null, null, null);
         int result = module.getNoOfSnapshotVersionsToRetain(config);
         Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void canConfigureHandlers()
+    {
+        List<Element> elements = Elements.getElements(module);
+        List<Element> privateElements = elements.stream()
+                .filter(e -> e instanceof PrivateElements)
+                .flatMap(e -> ((PrivateElements) e).getElements().stream())
+                .collect(Collectors.toList());
+
+        Map<Class<?>, Class<?>> linkedBindings = privateElements.stream()
+                .filter(e -> e instanceof LinkedKeyBinding)
+                .map(e -> (LinkedKeyBinding<?>) e)
+                .collect(Collectors.toMap(
+                        b -> b.getKey().getTypeLiteral().getRawType(),
+                        b -> b.getLinkedKey().getTypeLiteral().getRawType()
+                ));
+
+        Assertions.assertEquals(EntityProvider.class, linkedBindings.get(EntityArtifactsProvider.class));
+        Assertions.assertEquals(VersionedEntityProvider.class, linkedBindings.get(VersionedEntityArtifactsProvider.class));
+        Assertions.assertEquals(FileGenerationsProvider.class, linkedBindings.get(FileGenerationsArtifactsProvider.class));
+        Assertions.assertEquals(EntitiesHandlerImpl.class, linkedBindings.get(EntitiesArtifactsHandler.class));
+        Assertions.assertEquals(VersionedEntitiesHandlerImpl.class, linkedBindings.get(VersionedEntitiesArtifactsHandler.class));
+        Assertions.assertEquals(FileGenerationHandlerImpl.class, linkedBindings.get(FileGenerationsArtifactsHandler.class));
     }
 }
