dd5b54820bff19e39557119f8b4b3783ca53b4c7
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 13:51:50 +0000

    test(legend-depot-notifications-queue-mongo): add generated tests for org.finos.legend.depot.store.mongo.guice.NotificationsQueueMongoModule.configure()V

---
+++ legend-depot-notifications-queue-mongo/src/test/java/org/finos/legend/depot/store/mongo/guice/TestNotificationsQueueMongoModule.java
@@ -0,0 +1,50 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.store.mongo.guice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import com.mongodb.client.MongoDatabase;
+import org.finos.legend.depot.services.api.notifications.queue.Queue;
+import org.finos.legend.depot.store.mongo.TestStoreMongo;
+import org.finos.legend.depot.store.mongo.notifications.queue.NotificationsQueueMongo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestNotificationsQueueMongoModule extends TestStoreMongo
+{
+    @Test
+    void canConfigureModule()
+    {
+        Injector injector = Guice.createInjector(
+                new AbstractModule()
+                {
+                    @Override
+                    protected void configure()
+                    {
+                        bind(MongoDatabase.class).annotatedWith(Names.named("mongoDatabase")).toInstance(mongoProvider);
+                    }
+                },
+                new NotificationsQueueMongoModule()
+        );
+
+        Queue queue = injector.getInstance(Queue.class);
+        Assertions.assertNotNull(queue);
+        Assertions.assertTrue(queue instanceof NotificationsQueueMongo);
+    }
+}
