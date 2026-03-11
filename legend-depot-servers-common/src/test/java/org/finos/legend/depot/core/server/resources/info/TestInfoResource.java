0877329d28e69152952d4ace13fe9c418430a27d
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 15:19:58 +0000

    test(legend-depot-servers-common): add generated tests for org.finos.legend.depot.core.server.resources.info.InfoResource
    - <init>
    - getServerInfo
    - getServerConfig

---
+++ legend-depot-servers-common/src/test/java/org/finos/legend/depot/core/server/resources/info/TestInfoResource.java
@@ -0,0 +1,44 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.core.server.resources.info;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.finos.legend.depot.core.server.info.InfoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestInfoResource
+{
+    private final InfoService infoService = new InfoService();
+    private final InfoResource resource = new InfoResource(infoService, null);
+
+    @Test
+    void canGetServerInfo()
+    {
+        InfoService.ServerInfo serverInfo = resource.getServerInfo();
+
+        Assertions.assertNotNull(serverInfo);
+        Assertions.assertNotNull(serverInfo.getServerTimeZone());
+    }
+
+    @Test
+    void canGetServerConfig() throws JsonProcessingException
+    {
+        String config = resource.getServerConfig();
+
+        Assertions.assertEquals("null", config);
+    }
+}
