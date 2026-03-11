1ed566abd1e091c9da7637cf9e37eb9cedb8047e
Kevin Tse <kevin.tse@diffblue.com>
2026-03-11 14:24:17 +0000

    test(legend-depot-entities-api): add generated tests for org.finos.legend.depot.domain.entity.ProjectVersionEntities
    - <init>
    - <init>
    - isVersionedEntity
    - getEntities
    - equals
    - hashCode

---
+++ legend-depot-entities-api/src/test/java/org/finos/legend/depot/domain/entity/TestProjectVersionEntities.java
@@ -0,0 +1,77 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.domain.entity;
+
+import org.finos.legend.sdlc.domain.model.entity.Entity;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestProjectVersionEntities
+{
+    @Test
+    public void canCreateDefaultInstance()
+    {
+        ProjectVersionEntities pve = new ProjectVersionEntities();
+
+        assertNotNull(pve);
+        assertFalse(pve.isVersionedEntity());
+        assertNull(pve.getEntities());
+    }
+
+    @Test
+    public void canCreateInstanceWithParameters()
+    {
+        List<Entity> entities = Collections.emptyList();
+        ProjectVersionEntities pve = new ProjectVersionEntities("org.finos", "test-artifact", "1.0.0", entities);
+
+        assertEquals("org.finos", pve.getGroupId());
+        assertEquals("test-artifact", pve.getArtifactId());
+        assertEquals("1.0.0", pve.getVersionId());
+        assertFalse(pve.isVersionedEntity());
+        assertEquals(entities, pve.getEntities());
+    }
+
+    @Test
+    public void testEquals()
+    {
+        List<Entity> entities = Collections.emptyList();
+        ProjectVersionEntities pve1 = new ProjectVersionEntities("org.finos", "test-artifact", "1.0.0", entities);
+        ProjectVersionEntities pve2 = new ProjectVersionEntities("org.finos", "test-artifact", "1.0.0", entities);
+        ProjectVersionEntities pve3 = new ProjectVersionEntities("org.finos", "other-artifact", "1.0.0", entities);
+
+        assertEquals(pve1, pve2);
+        assertNotEquals(pve1, pve3);
+    }
+
+    @Test
+    public void testHashCode()
+    {
+        List<Entity> entities = Collections.emptyList();
+        ProjectVersionEntities pve1 = new ProjectVersionEntities("org.finos", "test-artifact", "1.0.0", entities);
+        ProjectVersionEntities pve2 = new ProjectVersionEntities("org.finos", "test-artifact", "1.0.0", entities);
+
+        assertEquals(pve1.hashCode(), pve2.hashCode());
+    }
+}
