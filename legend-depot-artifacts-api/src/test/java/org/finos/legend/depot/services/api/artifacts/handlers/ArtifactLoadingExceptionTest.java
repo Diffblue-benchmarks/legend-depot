aa83ec4763cdfbec02291460c330ad524754587e
Diffblue CI <ci@diffblue.com>
2026-03-13 19:46:31 +0000

    test(legend-depot-artifacts-api): add generated tests for org.finos.legend.depot.services.api.artifacts.handlers.ArtifactLoadingException.<init>(Ljava/lang/String;)V

---
+++ legend-depot-artifacts-api/src/test/java/org/finos/legend/depot/services/api/artifacts/handlers/ArtifactLoadingExceptionTest.java
@@ -0,0 +1,43 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.services.api.artifacts.handlers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ArtifactLoadingExceptionTest
+{
+    @Test
+    public void canCreateExceptionWithMessage()
+    {
+        String message = "Test error message";
+        ArtifactLoadingException exception = new ArtifactLoadingException(message);
+
+        assertNotNull(exception);
+        assertEquals(message, exception.getMessage());
+    }
+
+    @Test
+    public void testExceptionIsRuntimeException()
+    {
+        ArtifactLoadingException exception = new ArtifactLoadingException("Test");
+
+        assertTrue(exception instanceof RuntimeException);
+    }
+}
