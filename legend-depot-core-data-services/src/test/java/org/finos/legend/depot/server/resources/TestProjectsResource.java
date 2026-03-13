edad5569c43b5b40f8b2c70901c978a234049996
Diffblue CI <ci@diffblue.com>
2026-03-13 18:53:32 +0000

    test(legend-depot-core-data-services): add generated tests for org.finos.legend.depot.server.resources.projects.ProjectsResource
    - <init>
    - getProjectsWithCoordinates
    - getProjectCoordinates
    - getVersions

--- legend-depot-core-data-services/src/test/java/org/finos/legend/depot/server/resources/TestProjectsResource.java
+++ legend-depot-core-data-services/src/test/java/org/finos/legend/depot/server/resources/TestProjectsResource.java
@@ -19,12 +19,15 @@ import org.finos.legend.depot.server.resources.projects.ProjectsResource;
 import org.finos.legend.depot.services.TestBaseServices;
 import org.finos.legend.depot.services.projects.ProjectsServiceImpl;
 import org.finos.legend.depot.services.api.metrics.query.QueryMetricsRegistry;
+import org.finos.legend.depot.services.api.projects.ProjectsService;
 import org.finos.legend.depot.services.api.projects.configuration.ProjectsConfiguration;
 import org.finos.legend.depot.services.api.notifications.queue.Queue;
+import org.finos.legend.depot.store.model.projects.StoreProjectData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 
@@ -32,7 +35,8 @@ public class TestProjectsResource extends TestBaseServices
 {
     private final QueryMetricsRegistry metrics = mock(QueryMetricsRegistry.class);
     private final Queue queue = mock(Queue.class);
-    private ProjectsResource projectsVersionsResource = new ProjectsResource(new ProjectsServiceImpl(projectsVersionsStore, projectsStore, metrics, queue, new ProjectsConfiguration("master")));
+    private final ProjectsService projectsService = new ProjectsServiceImpl(projectsVersionsStore, projectsStore, metrics, queue, new ProjectsConfiguration("master"));
+    private ProjectsResource projectsVersionsResource = new ProjectsResource(projectsService);
 
     @Test
     public void canQueryVersionsForProjectGA()
@@ -48,5 +52,35 @@ public class TestProjectsResource extends TestBaseServices
         List<String> versionSet = (List<String>) projectsVersionsResource.getVersions("examples.metadata", "test",false).getEntity();
         Assertions.assertNotNull(versionSet);
         Assertions.assertEquals(2, versionSet.size());
+    }
+
+    @Test
+    public void canCreateProjectsResource()
+    {
+        ProjectsResource resource = new ProjectsResource(projectsService);
+        Assertions.assertNotNull(resource);
+    }
+
+    @Test
+    public void canGetProjectsWithCoordinates()
+    {
+        List<StoreProjectData> projects = (List<StoreProjectData>) projectsVersionsResource.getProjectsWithCoordinates().getEntity();
+        Assertions.assertNotNull(projects);
+        Assertions.assertFalse(projects.isEmpty());
+    }
+
+    @Test
+    public void canGetProjectCoordinates()
+    {
+        Optional<StoreProjectData> projectData = (Optional<StoreProjectData>) projectsVersionsResource.getProjectCoordinates("examples.metadata", "test").getEntity();
+        Assertions.assertNotNull(projectData);
+        Assertions.assertTrue(projectData.isPresent());
+    }
+
+    @Test
+    public void canQueryVersionsWithSnapshotsIncluded()
+    {
+        List<String> versionSet = (List<String>) projectsVersionsResource.getVersions("examples.metadata", "test", true).getEntity();
+        Assertions.assertNotNull(versionSet);
     }
 }
