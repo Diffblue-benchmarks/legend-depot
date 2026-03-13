65e0910b56ae63bb1d2770a31d30da3d1bdfe0d5
Diffblue CI <ci@diffblue.com>
2026-03-13 17:38:03 +0000

    test(legend-depot-model): add generated tests for org.finos.legend.depot.domain.DatesHandler
    - toTime
    - toDate
    - toDate
    - toDate
    - parseDate

--- legend-depot-model/src/test/java/org/finos/legend/depot/domain/DatesHandlerTest.java
+++ legend-depot-model/src/test/java/org/finos/legend/depot/domain/DatesHandlerTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DatesHandlerTest
 {
@@ -30,6 +31,58 @@ public class DatesHandlerTest
         LocalDateTime date = DatesHandler.parseDate("1679411706436");
         LocalDateTime lunchTime = LocalDateTime.parse("2023-03-21T14:02:49", DateTimeFormatter.ISO_DATE_TIME);
         Assertions.assertNotNull(date);
+
+    }
+
+    @Test
+    public void canConvertLocalDateTimeToTime()
+    {
+
+        LocalDateTime localDateTime = LocalDateTime.of(2023, 3, 21, 14, 2, 49);
+        long time = DatesHandler.toTime(localDateTime);
+        Assertions.assertTrue(time > 0);
+
+    }
+
+    @Test
+    public void canConvertDateToLocalDateTime()
+    {
+
+        Date date = new Date(1679411769000L);
+        LocalDateTime localDateTime = DatesHandler.toDate(date);
+        Assertions.assertNotNull(localDateTime);
+
+    }
+
+    @Test
+    public void canConvertLongToLocalDateTime()
+    {
+
+        long time = 1679411769000L;
+        LocalDateTime localDateTime = DatesHandler.toDate(time);
+        Assertions.assertNotNull(localDateTime);
+
+    }
+
+    @Test
+    public void canConvertLocalDateTimeToDate()
+    {
+
+        LocalDateTime localDateTime = LocalDateTime.of(2023, 3, 21, 14, 2, 49);
+        Date date = DatesHandler.toDate(localDateTime);
+        Assertions.assertNotNull(date);
+
+    }
+
+    @Test
+    public void canParseISODateTime()
+    {
+
+        LocalDateTime date = DatesHandler.parseDate("2023-03-21T14:02:49");
+        Assertions.assertNotNull(date);
+        Assertions.assertEquals(2023, date.getYear());
+        Assertions.assertEquals(3, date.getMonthValue());
+        Assertions.assertEquals(21, date.getDayOfMonth());
 
     }
 }
