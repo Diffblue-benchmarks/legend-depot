5d1c6e8e91a8d51d34c7f855045ad6e02d160ef6
Diffblue CI <ci@diffblue.com>
2026-03-13 17:45:37 +0000

    test(legend-depot-pure-model-context): add generated tests for org.finos.legend.depot.server.resources.guice.PureModelContextResourcesModule
    - configure
    - registerResourceMetrics

---
+++ legend-depot-pure-model-context/src/test/java/org/finos/legend/depot/server/resources/guice/TestPureModelContextResourcesModule.java
@@ -0,0 +1,64 @@
+//  Copyright 2022 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.server.resources.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.finos.legend.depot.core.services.api.metrics.PrometheusMetricsHandler;
+import org.finos.legend.depot.server.resources.pure.model.context.PureModelContextResource;
+import org.finos.legend.depot.services.api.pure.model.context.PureModelContextService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class TestPureModelContextResourcesModule
+{
+    @Test
+    public void canConfigureModule()
+    {
+        PureModelContextService mockService = mock(PureModelContextService.class);
+        PrometheusMetricsHandler mockMetricsHandler = mock(PrometheusMetricsHandler.class);
+
+        PureModelContextResourcesModule module = new PureModelContextResourcesModule();
+        Injector injector = Guice.createInjector(module, new AbstractModule()
+        {
+            @Override
+            protected void configure()
+            {
+                bind(PureModelContextService.class).toInstance(mockService);
+                bind(PrometheusMetricsHandler.class).toInstance(mockMetricsHandler);
+            }
+        });
+
+        PureModelContextResource resource = injector.getInstance(PureModelContextResource.class);
+        Assertions.assertNotNull(resource);
+    }
+
+    @Test
+    public void canRegisterResourceMetrics()
+    {
+        PureModelContextResourcesModule module = new PureModelContextResourcesModule();
+        PrometheusMetricsHandler metricsHandler = mock(PrometheusMetricsHandler.class);
+
+        boolean result = module.registerResourceMetrics(metricsHandler);
+
+        Assertions.assertTrue(result);
+        verify(metricsHandler).registerResourceSummaries(PureModelContextResource.class);
+    }
+}
