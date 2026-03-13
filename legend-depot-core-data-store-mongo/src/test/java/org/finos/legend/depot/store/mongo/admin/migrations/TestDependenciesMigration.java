8448bdff62695da0c9238042ec30ab276a384667
Diffblue CI <ci@diffblue.com>
2026-03-13 19:07:36 +0000

    test(legend-depot-core-data-store-mongo): add generated tests for org.finos.legend.depot.store.mongo.admin.migrations.DependenciesMigration.calculateTransitiveDependencies(Lorg/finos/legend/depot/domain/project/ProjectVersion;Lorg/eclipse/collections/api/block/function/Function3;)Lorg/finos/legend/depot/domain/project/dependencies/VersionDependencyReport;

--- legend-depot-core-data-store-mongo/src/test/java/org/finos/legend/depot/store/mongo/admin/migrations/TestDependenciesMigration.java
+++ legend-depot-core-data-store-mongo/src/test/java/org/finos/legend/depot/store/mongo/admin/migrations/TestDependenciesMigration.java
@@ -20,8 +20,10 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.eclipse.collections.api.block.function.Function3;
 import org.finos.legend.depot.domain.project.ProjectVersion;
 import org.finos.legend.depot.domain.project.ProjectVersionData;
+import org.finos.legend.depot.domain.project.dependencies.VersionDependencyReport;
 import org.finos.legend.depot.store.model.projects.StoreProjectVersionData;
 import org.finos.legend.depot.store.mongo.CoreDataMongoStoreTests;
 import org.finos.legend.depot.store.mongo.admin.CoreDataMigrations;
@@ -32,6 +34,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
@@ -255,5 +258,109 @@ public class TestDependenciesMigration extends CoreDataMongoStoreTests
         StoreProjectVersionData rootResult = convert(new ObjectMapper(), mongoProvider.getCollection(VERSIONS_COLLECTION).find(Filters.eq("artifactId", "root")).first(), StoreProjectVersionData.class);
         Assertions.assertTrue(rootResult.getTransitiveDependenciesReport().isValid());
         Assertions.assertEquals(2, rootResult.getTransitiveDependenciesReport().getTransitiveDependencies().size());
+    }
+
+    @Test
+    public void testDependencyOnExcludedProject()
+    {
+        mongoProvider.getCollection(ProjectsVersionsMongo.COLLECTION).drop();
+
+        StoreProjectVersionData excludedProject = new StoreProjectVersionData("test.group", "excluded-dep", "1.0.0");
+        ProjectVersionData excludedData = new ProjectVersionData();
+        excludedData.setExcluded(true);
+        excludedProject.setVersionData(excludedData);
+
+        StoreProjectVersionData projectDependingOnExcluded = new StoreProjectVersionData("test.group", "depends-on-excluded", "1.0.0");
+        ProjectVersionData dependingData = new ProjectVersionData();
+        dependingData.setDependencies(Arrays.asList(new ProjectVersion("test.group", "excluded-dep", "1.0.0")));
+        projectDependingOnExcluded.setVersionData(dependingData);
+
+        getMongoProjectVersions().insertOne(BaseMongo.buildDocument(excludedProject));
+        getMongoProjectVersions().insertOne(BaseMongo.buildDocument(projectDependingOnExcluded));
+
+        DependenciesMigration migration = new DependenciesMigration(mongoProvider);
+        migration.calculateTransitiveDependenciesForAllProjectVersions();
+
+        StoreProjectVersionData result = convert(new ObjectMapper(), mongoProvider.getCollection(VERSIONS_COLLECTION).find(Filters.eq("artifactId", "depends-on-excluded")).first(), StoreProjectVersionData.class);
+        Assertions.assertFalse(result.getTransitiveDependenciesReport().isValid());
+    }
+
+    @Test
+    public void testDependencyProviderThrowsException()
+    {
+        mongoProvider.getCollection(ProjectsVersionsMongo.COLLECTION).drop();
+
+        StoreProjectVersionData leaf = new StoreProjectVersionData("test.group", "leaf", "1.0.0");
+        leaf.setVersionData(new ProjectVersionData());
+
+        StoreProjectVersionData problematicMiddle = new StoreProjectVersionData("test.group", "problematic", "1.0.0");
+        ProjectVersionData problematicData = new ProjectVersionData();
+        problematicData.setDependencies(Arrays.asList(new ProjectVersion("test.group", "leaf", "1.0.0")));
+        problematicMiddle.setVersionData(problematicData);
+
+        StoreProjectVersionData root = new StoreProjectVersionData("test.group", "root", "1.0.0");
+        ProjectVersionData rootData = new ProjectVersionData();
+        rootData.setDependencies(Arrays.asList(new ProjectVersion("test.group", "problematic", "1.0.0")));
+        root.setVersionData(rootData);
+
+        getMongoProjectVersions().insertOne(BaseMongo.buildDocument(leaf));
+        getMongoProjectVersions().insertOne(BaseMongo.buildDocument(root));
+
+        DependenciesMigration migration = new DependenciesMigration(mongoProvider);
+        migration.calculateTransitiveDependenciesForAllProjectVersions();
+
+        StoreProjectVersionData result = convert(new ObjectMapper(), mongoProvider.getCollection(VERSIONS_COLLECTION).find(Filters.eq("artifactId", "root")).first(), StoreProjectVersionData.class);
+        Assertions.assertFalse(result.getTransitiveDependenciesReport().isValid());
+    }
+
+    @Test
+    public void testGenericExceptionInCalculateTransitiveDependencies() throws Exception
+    {
+        DependenciesMigration migration = new DependenciesMigration(mongoProvider);
+        ProjectVersion projectVersion = new ProjectVersion("test.group", "test-artifact", "1.0.0");
+
+        Function3<String, String, String, StoreProjectVersionData> throwingProvider = (group, artifact, version) -> {
+            throw new RuntimeException("Simulated unexpected error");
+        };
+
+        Method method = DependenciesMigration.class.getDeclaredMethod("calculateTransitiveDependencies", ProjectVersion.class, Function3.class);
+        method.setAccessible(true);
+
+        try
+        {
+            method.invoke(migration, projectVersion, throwingProvider);
+            Assertions.fail("Expected IllegalStateException to be thrown");
+        }
+        catch (Exception e)
+        {
+            Throwable cause = e.getCause();
+            Assertions.assertNotNull(cause);
+            Assertions.assertTrue(cause instanceof IllegalStateException);
+            Assertions.assertTrue(cause.getMessage().contains("Error finding transitive dependencies with message"));
+            Assertions.assertTrue(cause.getMessage().contains("Simulated unexpected error"));
+        }
+    }
+
+    @Test
+    public void testCalculateTransitiveDependenciesWithExcludedDependency() throws Exception
+    {
+        DependenciesMigration migration = new DependenciesMigration(mongoProvider);
+        ProjectVersion projectVersion = new ProjectVersion("test.group", "test-artifact", "1.0.0");
+
+        StoreProjectVersionData excludedData = new StoreProjectVersionData("test.group", "test-artifact", "1.0.0");
+        ProjectVersionData versionData = new ProjectVersionData();
+        versionData.setExcluded(true);
+        excludedData.setVersionData(versionData);
+
+        Function3<String, String, String, StoreProjectVersionData> excludedProvider = (group, artifact, version) -> excludedData;
+
+        Method method = DependenciesMigration.class.getDeclaredMethod("calculateTransitiveDependencies", ProjectVersion.class, Function3.class);
+        method.setAccessible(true);
+
+        VersionDependencyReport result = (VersionDependencyReport) method.invoke(migration, projectVersion, excludedProvider);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertFalse(result.isValid());
+        Assertions.assertTrue(result.getTransitiveDependencies().isEmpty());
     }
 }
