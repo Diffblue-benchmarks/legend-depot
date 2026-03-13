8cde5c4f0ed6784bbabe0aaac1647b537ae0a6f7
Diffblue CI <ci@diffblue.com>
2026-03-13 17:58:31 +0000

    test(legend-depot-core-schedules): add generated tests for org.finos.legend.depot.services.schedules.SchedulesFactoryImpl
    - <init>
    - registerExternalTriggerSchedule
    - registerSingleInstance
    - register
    - register
    - createScheduleInfo
    - createTimerTask
    - canExecute
    - deleteExpired
    - deRegister
    - deRegisterAll
    - trigger
    - run
    - toggleDisable
    - toggleDisableAll
    - execute

--- legend-depot-core-schedules/src/test/java/org/finos/legend/depot/services/schedules/TestSchedules.java
+++ legend-depot-core-schedules/src/test/java/org/finos/legend/depot/services/schedules/TestSchedules.java
@@ -109,4 +109,154 @@ public class TestSchedules
         Assertions.assertEquals(5,schedulesFactory.instancesStore.getAll().size());
     }
 
+    @Test
+    public void testConstructorWithHouseKeeper()
+    {
+        SchedulesFactoryImpl factory = new SchedulesFactoryImpl(new MockScheduleStore(), new MockInstancesStore(), true);
+        Assertions.assertNotNull(factory);
+        Assertions.assertNotNull(factory.schedulesStore);
+        Assertions.assertNotNull(factory.instancesStore);
+        factory.deRegisterAll();
+    }
+
+    @Test
+    public void testRegisterExternalTriggerSchedule()
+    {
+        schedulesFactory.registerExternalTriggerSchedule("externalJob", 5000L, () -> "external result");
+
+        Assertions.assertEquals(1, schedulesFactory.schedulesStore.getAll().size());
+        Assertions.assertTrue(schedulesFactory.schedulesStore.get("externalJob").isPresent());
+        Assertions.assertTrue(schedulesFactory.schedulesStore.get("externalJob").get().externalTrigger);
+    }
+
+    @Test
+    public void testTrigger()
+    {
+        schedulesFactory.register("triggerJob", 600000L, 100000L, () -> "trigger result");
+
+        schedulesFactory.trigger("triggerJob", false);
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+
+        schedulesFactory.trigger("nonExistent", false);
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+    }
+
+    @Test
+    public void testTriggerForceRun()
+    {
+        schedulesFactory.register("forceJob", 600000L, 100000L, () -> "force result");
+        schedulesFactory.toggleDisable("forceJob", true);
+
+        schedulesFactory.trigger("forceJob", false);
+        Assertions.assertEquals(0, schedulesFactory.instancesStore.getAll().size());
+
+        schedulesFactory.trigger("forceJob", true);
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+    }
+
+    @Test
+    public void testToggleDisable()
+    {
+        schedulesFactory.register("toggleJob", 600000L, 100000L, () -> "toggle result");
+
+        Assertions.assertFalse(schedulesFactory.schedulesStore.get("toggleJob").get().disabled);
+
+        schedulesFactory.toggleDisable("toggleJob", true);
+        Assertions.assertTrue(schedulesFactory.schedulesStore.get("toggleJob").get().disabled);
+
+        schedulesFactory.toggleDisable("toggleJob", false);
+        Assertions.assertFalse(schedulesFactory.schedulesStore.get("toggleJob").get().disabled);
+    }
+
+    @Test
+    public void testDeregisterAll()
+    {
+        schedulesFactory.register("job1", 600000L, 100000L, () -> "result1");
+        schedulesFactory.register("job2", 600000L, 100000L, () -> "result2");
+        schedulesFactory.register("job3", 600000L, 100000L, () -> "result3");
+
+        Assertions.assertEquals(3, schedulesFactory.schedulesStore.getAll().size());
+        Assertions.assertEquals(3, schedulesFactory.tasksRegistry.size());
+
+        schedulesFactory.deRegisterAll();
+
+        Assertions.assertEquals(0, schedulesFactory.schedulesStore.getAll().size());
+        Assertions.assertEquals(0, schedulesFactory.tasksRegistry.size());
+    }
+
+    @Test
+    public void testScheduleNotInStore()
+    {
+        schedulesFactory.register("tempJob", 600000L, 100000L, () -> "result");
+        Assertions.assertEquals(1, schedulesFactory.schedulesStore.getAll().size());
+
+        schedulesFactory.schedulesStore.delete("tempJob");
+
+        schedulesFactory.run("tempJob");
+
+        Assertions.assertEquals(0, schedulesFactory.tasksRegistry.size());
+    }
+
+    @Test
+    public void testDisabledScheduleSkipsExecution()
+    {
+        schedulesFactory.register("disabledJob", 600000L, 100000L, () -> "disabled result");
+        schedulesFactory.toggleDisable("disabledJob", true);
+
+        schedulesFactory.run("disabledJob");
+
+        Assertions.assertEquals(0, schedulesFactory.instancesStore.getAll().size());
+    }
+
+    @Test
+    public void testSingleInstanceCannotExecute()
+    {
+        schedulesFactory.registerSingleInstance("singleJob", 600000L, 100000L, () -> "single result");
+
+        schedulesFactory.run("singleJob");
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+
+        schedulesFactory.run("singleJob");
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+    }
+
+    @Test
+    public void testCanExecuteMethod()
+    {
+        schedulesFactory.register("execJob", 600000L, 100000L, () -> "exec result");
+
+        Assertions.assertTrue(schedulesFactory.canExecute("execJob"));
+
+        schedulesFactory.run("execJob");
+        Assertions.assertFalse(schedulesFactory.canExecute("execJob"));
+
+        ScheduleInstance instance = schedulesFactory.instancesStore.find("execJob").get(0);
+        instance.setExpires(toDate(LocalDateTime.now().minusMinutes(10)));
+
+        Assertions.assertTrue(schedulesFactory.canExecute("execJob"));
+    }
+
+    @Test
+    public void testExecuteWithNoFunction()
+    {
+        schedulesFactory.register("noFuncJob", 600000L, 100000L, () -> "result");
+        schedulesFactory.functions.remove("noFuncJob");
+
+        schedulesFactory.trigger("noFuncJob", true);
+
+        Assertions.assertEquals(0, schedulesFactory.instancesStore.getAll().size());
+    }
+
+    @Test
+    public void testExecuteWithException()
+    {
+        schedulesFactory.register("errorJob", 600000L, 100000L, () -> {
+            throw new RuntimeException("Test exception");
+        });
+
+        schedulesFactory.trigger("errorJob", true);
+
+        Assertions.assertEquals(1, schedulesFactory.instancesStore.getAll().size());
+    }
+
 }
