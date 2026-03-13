862d2e3cc48b4c9074ba67f4fa8dda2dd6e81a60
Diffblue CI <ci@diffblue.com>
2026-03-13 18:38:28 +0000

    test(legend-depot-notifications-store-mongo): add generated tests for org.finos.legend.depot.store.mongo.guice.NotificationsStoreMongoModule.registerIndexes(Lorg/finos/legend/depot/store/mongo/admin/MongoAdminStore;)Z

---
+++ legend-depot-notifications-store-mongo/src/test/java/org/finos/legend/depot/store/mongo/guice/TestNotificationsStoreMongoModule.java
@@ -0,0 +1,53 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.store.mongo.guice;
+
+import com.mongodb.client.model.IndexModel;
+import org.finos.legend.depot.store.mongo.TestStoreMongo;
+import org.finos.legend.depot.store.mongo.admin.MongoAdminStore;
+import org.finos.legend.depot.store.mongo.notifications.NotificationsMongo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TestNotificationsStoreMongoModule extends TestStoreMongo
+{
+    private NotificationsStoreMongoModule module = new NotificationsStoreMongoModule();
+
+    @Test
+    public void canRegisterIndexes()
+    {
+        MongoAdminStore adminStore = new MongoAdminStore(mongoProvider);
+
+        boolean result = module.registerIndexes(adminStore);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void registerIndexesCreatesCorrectIndexes()
+    {
+        MongoAdminStore adminStore = new MongoAdminStore(mongoProvider);
+
+        module.registerIndexes(adminStore);
+        List<String> createdIndexes = adminStore.createIndexes();
+
+        Assertions.assertNotNull(createdIndexes);
+        List<IndexModel> expectedIndexes = NotificationsMongo.buildIndexes();
+        Assertions.assertEquals(expectedIndexes.size(), createdIndexes.size());
+    }
+}
