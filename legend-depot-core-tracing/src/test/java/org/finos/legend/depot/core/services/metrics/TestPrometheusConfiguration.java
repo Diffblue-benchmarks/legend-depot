bcd43374a65b244ace4bb7ec737d1d5481a9113d
Diffblue CI <ci@diffblue.com>
2026-04-03 23:57:45 +0000

    test(legend-depot-core-tracing): Add generated tests for org.finos.legend.depot.core.services.api.metrics.configuration.PrometheusConfiguration
    - <init>
    - setEnabled
    - setMetricsHandler

---
+++ legend-depot-core-tracing/src/test/java/org/finos/legend/depot/core/services/metrics/TestPrometheusConfiguration.java
@@ -0,0 +1,49 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.core.services.metrics;
+
+import org.finos.legend.depot.core.services.api.metrics.configuration.PrometheusConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestPrometheusConfiguration
+{
+    @Test
+    public void testDefaultConstructor()
+    {
+        PrometheusConfiguration config = new PrometheusConfiguration();
+        Assertions.assertFalse(config.isEnabled());
+        Assertions.assertNull(config.getMetricsHandler());
+    }
+
+    @Test
+    public void testSetEnabled()
+    {
+        PrometheusConfiguration config = new PrometheusConfiguration();
+        config.setEnabled(true);
+        Assertions.assertTrue(config.isEnabled());
+    }
+
+    @Test
+    public void testSetMetricsHandler()
+    {
+        PrometheusConfiguration config = new PrometheusConfiguration();
+        DepotPrometheusMetricsHandler handler = new DepotPrometheusMetricsHandler("test");
+        config.setMetricsHandler(handler);
+        Assertions.assertNotNull(config.getMetricsHandler());
+        Assertions.assertEquals(handler, config.getMetricsHandler());
+    }
+}
