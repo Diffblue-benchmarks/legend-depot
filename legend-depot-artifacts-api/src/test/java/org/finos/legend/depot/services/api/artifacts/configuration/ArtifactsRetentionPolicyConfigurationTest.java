1aa3c1e2742c199e988f526aa15372ded821cd7f
Diffblue CI <ci@diffblue.com>
2026-04-03 23:13:28 +0000

    test(legend-depot-artifacts-api): Add generated tests for org.finos.legend.depot.services.api.artifacts.configuration.ArtifactsRetentionPolicyConfiguration
    - <init>
    - getMaximumSnapshotsAllowed
    - getTtlForVersions
    - getTtlForSnapshots

---
+++ legend-depot-artifacts-api/src/test/java/org/finos/legend/depot/services/api/artifacts/configuration/ArtifactsRetentionPolicyConfigurationTest.java
@@ -0,0 +1,53 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package org.finos.legend.depot.services.api.artifacts.configuration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArtifactsRetentionPolicyConfigurationTest
+{
+    @Test
+    public void testDefaultValues()
+    {
+        ArtifactsRetentionPolicyConfiguration config = new ArtifactsRetentionPolicyConfiguration(null, null, null);
+
+        assertEquals(5, config.getMaximumSnapshotsAllowed());
+        assertEquals(365, config.getTtlForVersions());
+        assertEquals(30, config.getTtlForSnapshots());
+    }
+
+    @Test
+    public void testCustomValues()
+    {
+        ArtifactsRetentionPolicyConfiguration config = new ArtifactsRetentionPolicyConfiguration(10, 180, 60);
+
+        assertEquals(10, config.getMaximumSnapshotsAllowed());
+        assertEquals(180, config.getTtlForVersions());
+        assertEquals(60, config.getTtlForSnapshots());
+    }
+
+    @Test
+    public void testPartialCustomValues()
+    {
+        ArtifactsRetentionPolicyConfiguration config = new ArtifactsRetentionPolicyConfiguration(3, null, null);
+
+        assertEquals(3, config.getMaximumSnapshotsAllowed());
+        assertEquals(365, config.getTtlForVersions());
+        assertEquals(30, config.getTtlForSnapshots());
+    }
+}
